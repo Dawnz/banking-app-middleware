d8df1c1174332c81fe8382feb93bd2ca5a8161b5
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-27 12:03:35 -0500

    created api folder with the first version of the api

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 8000;
+const APIVERSION1 = require("./API/api-v1");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
+
+app.use("api/v1", APIVERSION1),
 
 app.listen(PORT, ()=>{
     console.log('listening on port ', PORT);
