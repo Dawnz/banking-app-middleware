415654836e568f85238531efab311b1bf62e327a
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-27 11:54:58 -0500

    fixed unimported modules for cors and configured env variables

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
