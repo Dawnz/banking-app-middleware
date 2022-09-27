32614bf62d28f77f58184e7538822f82f5e0ce9b
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-27 12:10:55 -0500

    updated index.js with connection to the mongoose

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 8000;
 const APIVERSION1 = require("./API/api-v1");
+const mongoose = require("mongoose");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -11,6 +12,10 @@ app.use(cors());
 
 
 app.use("api/v1", APIVERSION1),
+
+mongoose.connect(process.env.DB_URL,{},()=>{
+    console.log("database connection established");
+})
 
 app.listen(PORT, ()=>{
     console.log('listening on port ', PORT);
