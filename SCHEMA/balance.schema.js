1f05cf63a8cc52b92c10d437fa99090637096b73
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-28 12:24:38 -0500

    created fixed error with missing {

--- SCHEMA/balance.schema.js
+++ SCHEMA/balance.schema.js
@@ -2,7 +2,6 @@ const {model, Schema} = require("mongoose");
 
 const balanceSchema = new Schema({
     account_balance: {type:Number, required: [true, "An account balance should be present at all times"], default: 0}
-
 }, {timestamps:true});
 
 /**
@@ -12,9 +11,9 @@ const balanceSchema = new Schema({
  */
 balanceSchema.methods.calculateBalance = async function(data){
     if(!data.transaction_amount) throw new Error("Amount is needed to make transaction");
-    if(data.transaction_type == "WITHDRAW")
+    if(data.transaction_type == "WITHDRAW"){
         this.account_balance -= data.transaction_amount;
-    else if(data.transaction_type == "DEPOSIT"){
+    }else if(data.transaction_type == "DEPOSIT"){
         this.account_balance -= data.transaction_amount;
     }
     this.save();
