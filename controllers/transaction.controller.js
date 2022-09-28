dafcbf284cbd84ef3aa6b5f4803b2480f671d55f
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-28 11:35:44 -0500

    created crud in controller for transaction

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -23,6 +23,58 @@ class TransactionController {
         }
     }
 
+
+    static getAllTransactions = async (req, res) => {
+        try{
+            if(req.query.account_id){
+                return this.getAllAccountTransactions(req, res, next);
+            }else{
+               let transactions = await Transaction.find();
+               JSONResponse.success(transactions, "Successfully retrieved all transactions", transactions, 200);
+            }
+        }catch(error){
+            JSONResponse.success(res, "Unable to retrieve transactions", error, 404);
+        }
+    }
+
+    static getAllAccountTransactions = async (req, res, next)=>{
+        try{
+            let {account_id} = req.query.account_id;
+            if(account_id){
+                let transactions = await Transaction.find({account_id: account_id});
+                JSONResponse.success(res, "Successfully retrieved all transactions for account", transactions, 200);
+            }
+        }catch(error){
+            JSONResponse.error(res, "Unable to retrieve account transactions", error, 404);
+        }
+    }
+
+    static getTransactionById = async (req, res, next)=>{
+        try{
+            let id = req.query.id;
+            if(ObjectId.isValid(id)) throw new Error("Invalid ID for transaction, check the ID");
+            let transaction = await Transaction.findById(id);
+            JSONResponse.success(res, "Successfully retrieved transaction", transaction, 200);
+        }catch(error){
+            JSONResponse.error(res, "Unable to retrieve transaction", error, 404);
+        }
+    }
+
+    static updateTransactionStatus = async (req, res, next)=>{
+        try{
+            let id = req.query.id;
+            let data = req.body;
+            if(ObjectId.isValid(id)) throw new Error("Invalid ID for transcation, check the ID");
+            if(Object.keys(data).length == 0) return JSONResponse.success(res,"No data passed to update the transaction, transaction not updated", data, 200);
+
+            let transaction = await Transaction.findById(id);
+            transaction.transaction_status = data.transaction_status;
+            JSONResponse.success(res, "Successfully updated transaction", transaction, 200);
+        }catch(error){
+            JSONResponse.error(res, "Unable to update transaction", error,404);
+        }
+    }
+
     /**
      * ### Description
      * Compares the names on the account and on the potential transation to see if the user owns the account.
