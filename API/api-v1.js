1c95586a8c83c995ecb48075c79136066608692f
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-27 20:11:17 -0500

    created route for authentication

--- API/api-v1.js
+++ API/api-v1.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router("/accounts", require("../routes/account.routes"));
 router("/users", require("../routes/user.routes"));
-
+router("/authenticate", require("../routes/auth.routes"));
 
 
 module.exports = router;
