78691d287071514d2ddac48ac0b1fbbf0d1403a0
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-29 05:11:38 -0500

    added middleware to the user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 const UserController = require('../controllers/user.controller');
+const Middleware = require('../middlewares/middleware');
 router
     .route("/")
-    .get(UserController.getAllUsers)
+    .get(Middleware.isAuthenticated,UserController.getAllUsers)
     .post(UserController.createUserProfile)
     
 router
     .route("/:id")
-    .get(UserController.getUserProfile)
-    .patch(UserController.updateUserProfile)
-    .delete(UserController.deleteUserProfile)
+    .get(Middleware.isAuthenticated, UserController.getUserProfile)
+    .patch(Middleware.isAuthenticated, UserController.updateUserProfile)
+    .delete(Middleware.isAuthenticated, UserController.deleteUserProfile)
     
 module.exports = router;
