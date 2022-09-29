e4fa95001853eef82b14a43857a420603a8d9ce8
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-29 08:44:21 -0500

    updated the transaction to use Balance schema instead of Account and wrapped all the await inside a try catch block

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -1,21 +1,31 @@
 const Transaction = require("../schema/transaction.schema");
 const Account  = require("../schema/account.schema");
+const Balance  = require("../schema/balance.schema");
 const { JSONResponse } = require("../utilities/jsonResponse");
 const { ObjectId } = require("mongoose").Types;
 
 
 class TransactionController {
 
+            /**
+     *  ### Description
+     * Creates a Transaction of either a withdrawal or a deposit.
+     * @param {Request} req 
+     * @param {Response} res 
+     * @param {*} next 
+     * @returns 
+     */
     static createTransaction = async(req, res, next) =>{
         try{
             let data = req.body;
             let account_number = req.body;
+            data.transaction_type = data.transaction_type.toUpperCase();
             if(Object.keys(data).length == 0) throw new Error("No data passed to create transaction");
-            let account = await Account.find({account_number: account_number})[0];
-
-            if(!(data.transaction_type.toUpperCase() == "WITHDRAW" && this.isUserAccount(data, account))) throw new Error("User is unauthorized to make this transaction");
-
-            this.make_transaction(data, account);
+            let account = await Account.findOne({account_number: account_number});
+            if(!account) throw new Error("No account found with this account number");
+            if(!(data.transaction_type === "WITHDRAW" && this.isUserAccount(data, account))) throw new Error("User is unauthorized to make this transaction");
+            let balance = this.getBalanceFromAccount(account.account_balance);
+            this.makeTransaction(data, balance);
             let transaction = await Transaction.create(data);
             JSONResponse.success(res, "Successfully made transaction", transaction, 201 );
         }catch(error){
@@ -23,7 +33,14 @@ class TransactionController {
         }
     }
 
-
+            /**
+     *  ### Description
+     * Retrieves all transactions from the database.
+     * @param {Request} req 
+     * @param {Response} res 
+     * @param {*} next 
+     * @returns 
+     */
     static getAllTransactions = async (req, res) => {
         try{
             if(req.query.account_id){
@@ -37,6 +54,14 @@ class TransactionController {
         }
     }
 
+            /**
+     *  ### Description
+     * Retrieves all Transactions of a user which matches the account id passed in the request's query parameters.
+     * @param {Request} req 
+     * @param {Response} res 
+     * @param {*} next 
+     * @returns 
+     */
     static getAllAccountTransactions = async (req, res, next)=>{
         try{
             let {account_id} = req.query.account_id;
@@ -48,18 +73,34 @@ class TransactionController {
             JSONResponse.error(res, "Unable to retrieve account transactions", error, 404);
         }
     }
-
+        /**
+     *  ### Description
+     * Retrieves a transaction finding it by its ID.
+     * @param {Request} req 
+     * @param {Response} res 
+     * @param {*} next 
+     * @returns 
+     */
     static getTransactionById = async (req, res, next)=>{
         try{
             let id = req.query.id;
             if(ObjectId.isValid(id)) throw new Error("Invalid ID for transaction, check the ID");
             let transaction = await Transaction.findById(id);
+            if(!transaction) throw new Error("No transaction found with that ID")
             JSONResponse.success(res, "Successfully retrieved transaction", transaction, 200);
         }catch(error){
             JSONResponse.error(res, "Unable to retrieve transaction", error, 404);
         }
     }
 
+    /**
+     *  ### Description
+     * Updated a transaction finding it by its ID. The function only facilitates updating the status of the transaction. 
+     * @param {Request} req 
+     * @param {Response} res 
+     * @param {*} next 
+     * @returns 
+     */
     static updateTransactionStatus = async (req, res, next)=>{
         try{
             let id = req.query.id;
@@ -68,6 +109,8 @@ class TransactionController {
             if(Object.keys(data).length == 0) return JSONResponse.success(res,"No data passed to update the transaction, transaction not updated", data, 200);
 
             let transaction = await Transaction.findById(id);
+            if(!transaction) throw new Error("No transaction found with that ID")
+
             transaction.transaction_status = data.transaction_status;
             JSONResponse.success(res, "Successfully updated transaction", transaction, 200);
         }catch(error){
@@ -96,12 +139,37 @@ class TransactionController {
      * @param {Account} account - Data of account based on the account information that was retrieved from the database.
      * @returns 
      */
-    static makeTransaction = async(data, account)=>{
+    static makeTransaction = async(data, balance)=>{
+        try{
+
+
         let ammount = data.transaction_ammount;
         if(!ammount) throw new Error("No transaction ammount was given");
-        if(account.account_balance < ammount && data.transaction_type == "WITHDRAW") throw new Error("Insufficient funds to complete transaction");
+        if(balance.account_balance < ammount && data.transaction_type == "WITHDRAW") throw new Error("Insufficient funds to complete transaction");
         
-        await account.calculateBalance(data);
+        await balance.calculateBalance(data);
+        
+        }catch(error){
+            throw new Error(error);
+        }
+    }
+
+
+    /**
+     * ### Description
+     * Returns a balance document which matches the id of the one passed in as parameter to the function.
+     * @param {string} balance_id This represents an objectId that is referenced from the account schema
+     * @returns {Balance}
+     */
+    static getBalanceFromAccount = async(balance_id)=>{
+        try{
+            let balance = await Balance.findById(balance_id);
+            if(!balance) throw new Error("Unable to retrieve balance from this account");
+            return balance;
+        }catch(error){
+            throw new Error(error)
+        }
+
     }
 }
 
