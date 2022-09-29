8ce1183420b72b2905c20d369d7808698c305ec0
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-29 04:00:15 -0500

    created random number generator and added middleware and schema method to ensure that all account numbers assigned are unique

--- SCHEMA/account.schema.js
+++ SCHEMA/account.schema.js
@@ -1,4 +1,6 @@
 const {model, Schema} = require("mongoose");
+const { randomNumberGenerator } = require("../utilities/randomNumbersGenerator");
+
 
 const accountSchema = new Schema({
     account_type : {
@@ -8,6 +10,9 @@ const accountSchema = new Schema({
         message: "{VALUE} is not a correct account type, it should be either 'SAVINGS' or 'CHECKING'"
         }   
     },
+    account_number: {
+        type: String,
+    },
     account_currency: {
         type: String, 
         max: [3, "The currency name should be 3 characters long"],
@@ -15,7 +20,12 @@ const accountSchema = new Schema({
     },
     id_type: {
         type: String,
-        required: [true, "Type of ID must be present in order to be valid"]
+        required: [true, "Type of ID must be present in order to be valid"],
+        enum: {
+            values: ["NATIONAL", "PASSPORT", "DRIVER"],
+            message: "{VALUE} is not valid, it should be either 'NATIONAL', 'PASSPORT', 'DRIVER'"
+        },
+        uppercase: true,
     },
     id_number: {
         type: String,
@@ -48,5 +58,40 @@ const accountSchema = new Schema({
     }
 
 });
+const Account = model("Account", accountSchema);
+module.exports = Account;
 
-module.exports = model("Account", accountSchema);
+
+/**
+ * ### Description
+ * This schema static method compares an accountNumber passed in to all the accounts in the database. If there is an account with that number, it will return false. Otherwise, it will return true.
+ * @param {string} accountNumber 
+ * @returns {boolean}
+ */
+accountSchema.statics.compareAccountNumbers = async function(accountNumber){
+    const Account = this;
+    const accounts =  await Account.find({account_number: accountNumber});
+    if(accounts.length > 0){
+        return false;
+    }
+    return true;
+
+}
+
+/**
+ * ### Description
+ * This Middleware will run after an account have been saved. Its purpose is to asign an account number to the document.
+ */
+accountSchema.post('save', async function(doc){
+    let prefix = doc._id.toString().slice(0, 3);
+    let accountNumber = randomNumberGenerator(prefix, 8);
+    while(true){
+        if(await Account.compareAccountNumbers(accountNumber)){
+            doc.account_number = accountNumber;
+            break;
+        }
+    }
+    return;
+
+})
+
