3244e76ed96a0f58810db5490195e8a286d401ad
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-29 10:48:08 -0500

    fixed issue with custom error messages

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -9,12 +9,12 @@ class AuthController{
             let {username, password} = req.body;
             let user = await User.findOne({username: username});
             if(!user) throw new Error("No user present which matches the username");
-            if(!(await user.isCorrectPassword(password))) throw new Error("Invalid password");
-            user
+            let passCheck = await user.isCorrectPassword(password);
+            if(!passCheck) throw new Error("Invalid password");
             let data = user;
             data.password = undefined;
             let token = generateJWTToken({id: user._id, username: user.username}, "3600");
-            
+
             JSONResponse.success(res,"User is authenticated successfully", {user, token}, 200);
         }catch(error){
             JSONResponse.error(res,"User not Authenticated", error, 404);
