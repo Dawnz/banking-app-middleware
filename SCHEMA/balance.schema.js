20e6fec619655d0077d499c52d997a0a7a3520f6
aldaine-AG <aldaineclarke1@gmail.com>
2022-10-01 15:05:21 -0500

    workable version of the balance schema file

--- SCHEMA/balance.schema.js
+++ SCHEMA/balance.schema.js
@@ -7,16 +7,24 @@ const balanceSchema = new Schema({
 /**
  * ### description
  * Calculate the account balance using the data that passed into the instance method property on the balance schema
- * @param {Transaction} data 
+ * 
  */
 balanceSchema.methods.calculateBalance = async function(data){
-    if(!data.transaction_amount) throw new Error("Amount is needed to make transaction");
-    if(data.transaction_type == "WITHDRAW"){
-        this.account_balance -= data.transaction_amount;
-    }else if(data.transaction_type == "DEPOSIT"){
-        this.account_balance -= data.transaction_amount;
+    try{
+        if(!data.transaction_amount) throw new Error("Amount is needed to make transaction");
+        if(data.transaction_type == "WITHDRAW"){
+            this.account_balance -= data.transaction_amount;
+        }else if(data.transaction_type == "DEPOSIT"){
+            this.account_balance += data.transaction_amount;
+        }
+        return await this.save();
+    }catch(err){
+        console.log("There was an error");
+        throw new Error(err.message);
     }
-    this.save();
+
+
+    
 }
 
 module.exports = model("Balance", balanceSchema);
