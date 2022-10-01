ef873c5514a0009d2ec167813046d3767c7640bb
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-30 21:48:05 -0500

    removed account schema middlewares

--- SCHEMA/account.schema.js
+++ SCHEMA/account.schema.js
@@ -1,7 +1,6 @@
 const {model, Schema} = require("mongoose");
-const { randomNumberGenerator } = require("../utilities/randomNumbersGenerator");
-
-
+const {ObjectId} = require("mongoose").Types; 
+const Balance = require("../schema/balance.schema");
 const accountSchema = new Schema({
     account_type : {
         type: String, 
@@ -17,13 +16,14 @@ const accountSchema = new Schema({
         type: String, 
         max: [3, "The currency name should be 3 characters long"],
         min: [3, "The currency name should be 3 characters long"],
+        default: "JMD"
     },
     id_type: {
         type: String,
         required: [true, "Type of ID must be present in order to be valid"],
         enum: {
-            values: ["NATIONAL", "PASSPORT", "DRIVER"],
-            message: "{VALUE} is not valid, it should be either 'NATIONAL', 'PASSPORT', 'DRIVER'"
+            values: ["NATIONAL", "PASSPORT", "LICENSE"],
+            message: "{VALUE} is not valid, it should be either 'NATIONAL', 'PASSPORT', 'LICENSE'"
         },
         uppercase: true,
     },
@@ -59,39 +59,7 @@ const accountSchema = new Schema({
 
 }, {timestamps:true});
 const Account = model("Account", accountSchema);
+
+
+
 module.exports = Account;
-
-
-/**
- * ### Description
- * This schema static method compares an accountNumber passed in to all the accounts in the database. If there is an account with that number, it will return false. Otherwise, it will return true.
- * @param {string} accountNumber 
- * @returns {boolean}
- */
-accountSchema.statics.compareAccountNumbers = async function(accountNumber){
-    const Account = this;
-    const accounts =  await Account.find({account_number: accountNumber});
-    if(accounts.length > 0){
-        return false;
-    }
-    return true;
-
-}
-
-/**
- * ### Description
- * This Middleware will run after an account have been saved. Its purpose is to asign an account number to the document.
- */
-accountSchema.post('save', async function(doc){
-    let prefix = doc._id.toString().slice(0, 3);
-    let accountNumber = randomNumberGenerator(prefix, 8);
-    while(true){
-        if(await Account.compareAccountNumbers(accountNumber)){
-            doc.account_number = accountNumber;
-            break;
-        }
-    }
-    return;
-
-})
-
